04375283c0eaa707665f4484039ff3c4a991c94b
napun87 <accmaruthunhat@gmail.com>
2026-08-19 13:28:05 +0700

    Update 1.OUROBOROS.lua

--- 1.OUROBOROS.lua
+++ 1.OUROBOROS.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/joustingmatch/Ouroboros/main/loader.lua"))()
+loadstring(game:HttpGet("https://robloxscriptbest.com/script/loader.php?script=lon"))()
